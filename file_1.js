f9f782c727ab8455eb549e6da7f128c939071783
Roman_Onishchuk <romanonisuk@gmail.com>
2022-07-12 00:37:07 +0300

    Homework module 5

---
+++ file_1.js
@@ -0,0 +1,11 @@
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const qwe of clients) {
+//   console.log(qwe);
+// }
+
+// const string = "dgshjgfdhfgsjdhfg";
+
+// for (const qweqwe of string) {
+//   console.log(qweqwe);
+// }
